bd5fb69f8cd6f865ba7842bdada8b1e8ff55cbdd
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-05-14 21:36:47 +0100

    add to database

--- database-script-0x02/seed.sql
+++ database-script-0x02/seed.sql
@@ -1,11 +1,32 @@
-INSERT INTO User(id, first_name, last_name, email, password_hash, phone_number, user_role) VALUES 
-(1, 'John', 'Doe', 'john.doe@example.com', 'hashed_password_1', '1234567890', 'guest')
+/*Create SQL scripts to populate the database with sample data for the AirBnB database.*/
 
-INSERT INTO Property(id, host_id, property_description, property_name, property_location, price_per_night) VALUES
-(1, 1, 'A cozy apartment in the city center', 'City Apartment', 'New York', 120.00)
+-- Insert sample data into the users table
+INSERT INTO User(id, first_name, last_name, email, password_hash, phone_number, user_role)
+ VALUES 
+    (1, 'John', 'Doe', 'john.doe@example.com', 'password1234', '1234567890', 'guest'),
+    (2, 'bob', 'bill', 'bob.bil@example.com', 'password1234', 12345678, 'guest');
 
-INSERT INTO Booking(id, property_id, user_id, start_date, end_date, total_price, status) VALUES
-(1, 1, 1, "14-4-2025", "15-4-2025", 120.00, canceled)
+-- Insert sample data into the properties table
+INSERT INTO Property(id, host_id, property_description, property_name, property_location, price_per_night)
+ VALUES
+    (1, 1, 'A cozy apartment in the city center', 'City Apartment', 'New York', 120.00),
+    (2, 1, 'A beautiful beach house with ocean views.', 'Beach house' , 'Miami, FL', 300.00);
 
-INSERT INTO Payment(id, booking_id, amount, payment_method) VALUES(1, 1, 120.00, "paypal")
+--Insert sample data into the booking table
+INSERT INTO Booking(id, property_id, user_id, start_date, end_date, total_price, status)
+ VALUES
+    (1, 1, 1, '14-4-2025', '15-4-2025', 1200.00, 'canceled'),
+    (2, 2, 2, '2023-11-01', '2023-11-10', 3000.00, 'pending');
 
+--Insert sample data into the payment table
+INSERT INTO Payment(id, booking_id, amount, payment_method)
+ VALUES
+    (1, 1, 1200.00, 'paypal'),
+    (2, 2, 3000.00. 'paypal');
+
+--Insert sample data into the review table
+
+INSERT INTO Review(id, property_id, user_id, rating comment)
+VALUES
+    (1, 1, 2, 5, 'five star rating for this property'),
+    (2, 2, 1, 4, 'nice place but need maintainance');
