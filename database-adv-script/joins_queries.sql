17c8c00173db18a96e4ea138e2a2500f6e5b09ea
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-05-15 22:10:34 +0100

    add Query script

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,16 @@
+/* Create a SQL script to write different queries using different types of joins*/
+
+--INNER JOIN to retrieve all bookings and the respective users who made those bookings.
+SELECT Booking.booking_id, User.first_name, User.last_name
+FROM Booking
+INNER JOIN User ON Booking.user_id = User.user_id;
+
+--LEFT JOIN to retrieve all properties and their reviews
+SELECT Property.property_id, Property.property_name, Review.comment
+FROM Property
+LEFT JOIN Review ON  Property.property_id = Review.property_id;
+
+--FULL OUTER JOIN to retrieve all users and all bookings
+SELECT Booking.booking_id, User.first_name, User.last_name
+FROM Booking
+FULL JOIN User ON Booking.user_id = User.user_id;
