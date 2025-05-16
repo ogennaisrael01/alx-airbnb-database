01a5431e78b21f9dd99950482f2c818f5e16a249
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-05-16 15:55:57 +0100

    partiton booking by start date

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -1,29 +1,17 @@
 -- Partition the Booking table by RANGE on the start_date column;
 
-PARTITION BY  RANGE (YEAR(start_date))(
-    PARTITION p2023 VALUES LESS THAN (2024)
+CREATE TABLE Booking (
+    id INT PRIMARY KEY,
+    property_id INT,
+    user_id INT,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL(10,2) NOT NULL,
+    status ENUM('pending', 'confirmed', 'canceled') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+PARTITION BY RANGE (YEAR(start_date))(
+    PARTITION p2023 VALUES LESS THAN (2024),
     PARTITION P2024 VALUES LESS THAN (2025)
 
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-PARTITION BY RANGE (YEAR(start_date)) (
-    PARTITION p2023 VALUES LESS THAN (2024),
-    PARTITION p2024 VALUES LESS THAN (2025)
