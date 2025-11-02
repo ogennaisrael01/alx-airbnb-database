57c7d63367970d9a391dc397dd06ab2178b6b040
ogennnaisrael01 <ogennaisrael98@gmail.com>
2025-11-02 11:16:32 +0100

    feat: implemeted a corelated subquery to filter users who have made up to 3 bookings

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,12 +1,15 @@
 -- -- Write a query to find all properties where the average rating is greater than 4.0 using a subquery.
 
--- SELECT product_id,
---     rating, message
--- FROM(
---     SELECT * 
---     FROM reviews
---     WHERE rating > 4.0
--- )
+SELECT product_id,
+    ROUND(AVG(rating), 2) as avg_rating, message
+FROM(
+    SELECT product_id, rating, message
+    FROM reviews
+    GROUP BY product_id, rating, message
+    HAVING AVG(rating) > 4.0
+)
+GROUP BY product_id, rating, message
+
 
 -- -- Write a correlated subquery to find users who have made more than 3 bookings
 SELECT users.id, CONCAT(users.first_name, ' ', users.last_name) as full_name,
@@ -14,6 +17,7 @@ SELECT users.id, CONCAT(users.first_name, ' ', users.last_name) as full_name,
                     COUNT(bookings.user_id) FROM bookings
                     WHERE bookings.user_id = users.id
                     GROUP BY bookings.user_id
+                    
                     
                 ) AS total_bookings
 FROM users
