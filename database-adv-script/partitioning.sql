585179c256c2783a0bc7745caece1c77d8dd027c
ogennnaisrael01 <ogennaisrael98@gmail.com>
2025-11-02 13:07:43 +0100

    feat: implemented partitioning to improve filtering of large dataset

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -2,9 +2,7 @@ CREATE TABLE Booking_2025 PARTITION OF bookings
     FOR VALUES FROM ('2025-01-01') TO ('2026-01-01');
 
 
-SELECT * 
+SELECT *,
+    ROW_NUMBER() OVER (PARTITION BY bookings.start_date)
 FROM bookings
-WHERE bookings.start_date >= '2025-01-01'
-    AND bookings.start_date < '2026-01-01'
-WHERE bookings.status = 'confirmed'
-ORDER BY bookings.start_date 
+
